dfcdea345d5e00fbf8b9555945eb199c35e42940
M Kashif Irshad <kashif.ir@gamil.com>
2015-06-28 12:44:09 +0500

    orders

--- application/modules/admin/controllers/AdminController.php
+++ application/modules/admin/controllers/AdminController.php
@@ -5,6 +5,24 @@ class Admin_AdminController extends Zend_Controller_Action {
     public function indexAction() {
         
     }
+    
+    public function ordersAction(){
+         $auth = Zend_Auth::getInstance();
+        $auth->setStorage(new Zend_Auth_Storage_Session('user'));
+        //check if user is logged in
+        if ($auth->hasIdentity()) {
+            $session = new Zend_Session_Namespace('userObj');
+            $userObj = $session->__get('userObj');
+            // This page only visible to operator
+            if($userObj['user_type'] == 'operator'){
+                
+            }else{
+                $this->_redirect('dashboard');
+            }
+        }else{
+            $this->_redirect('dashboard');
+        }
+    }
 
     public function manageuserAction() {
         $model = new Application_Model_User();
