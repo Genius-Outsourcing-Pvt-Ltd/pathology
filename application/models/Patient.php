2b5929fd58e2f78e950c079247242f15feac6bb1
bilalshakeel <medeveloper2@gmail.com>
2015-06-28 23:14:50 +0500

    downloaded pdf

--- application/models/Patient.php
+++ application/models/Patient.php
@@ -10,7 +10,7 @@ class Application_Model_Patient extends Application_Model_DbTable_Patient {
         $obj = new Zend_Db_Select(Zend_Db_Table::getDefaultAdapter());
         $select = $obj->from('patients as p', array('id'))
                 ->join('users as u', 'p.user_id=u.id', array(new Zend_Db_Expr('CONCAT(first_name," ",last_name) as name')))
-                ->join('patient_orders as po', 'u.id=po.patient_id', array('id as order_id', 'total_tests', 'total_results_calculated','created_at'))
+                ->join('patient_orders as po', 'u.id=po.user_id', array('id as order_id', 'total_tests', 'total_results_calculated','created_at'))
                 ->join('order_tests as ot', 'po.id=ot.order_id',array('results'))
                 ->join('tests as t', 't.id=ot.test_id', array('name as test_name'))
                 ->group('po.id');
@@ -25,9 +25,9 @@ class Application_Model_Patient extends Application_Model_DbTable_Patient {
 
     public static function getPatientById($id) {
         $obj = new Zend_Db_Select(Zend_Db_Table::getDefaultAdapter());
-        $select = $obj->from('patients as p', array('id'))
-                ->join('users as u', 'p.user_id=u.id', array(new Zend_Db_Expr('CONCAT(first_name," ",last_name) as name')))
-                ->join('patient_orders as po', 'u.id=po.patient_id', array('id as order_id', 'total_tests', 'total_results_calculated','created_at'))
+        $select = $obj->from('patients as p', array('id','ref_by_doctor','m_r_no','created_at as date_in'))
+                ->join('users as u', 'p.user_id=u.id', array(new Zend_Db_Expr('CONCAT(first_name," ",last_name) as name'),'u.*','u.id as userId'))
+                ->join('patient_orders as po', 'u.id=po.user_id', array('id as order_id', 'total_tests', 'total_results_calculated','created_at'))
                 ->join('order_tests as ot', 'po.id=ot.order_id',array('results','test_id as test_id'))
                 ->join('tests as t', 't.id=ot.test_id', array('name as test_name'))
                 ->where('p.id=?',$id);
--- application/modules/patient/controllers/PatientController.php
+++ application/modules/patient/controllers/PatientController.php
@@ -1,5 +1,7 @@
 <?php
+
 use Application_Model_Patient as patient;
+
 class Patient_PatientController extends Zend_Controller_Action {
 
     public function indexAction() {
@@ -8,14 +10,18 @@ class Patient_PatientController extends Zend_Controller_Action {
 
     public function viewreportAction() {
         $id = $this->getRequest()->getParam('id', '');
-        $patient = patient::getPatientById($id);
-//        print_r($patient); die;
+        $this->view->patients = $patient = patient::getPatientById($id);
+        $age = '';
+        if(!empty($patient[0]['birthday']) && $patient[0]['birthday'] != ''){
+            $age = $this->calculatePatientAge($patient[0]['birthday']);
+        }
+        $this->view->age = $age;
     }
 
     public function downloadpdfAction() {
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
-        
+
         require_once 'mpdf/mpdf.php';
         $html .= $this->view->action('viewreport', 'patient', 'patient', array('id' => '1'));
         $mpdf = new mPDF('+aCJK', 'A4', '', '', 15, 15, 15, 0, 0, 0);
@@ -25,6 +31,13 @@ class Patient_PatientController extends Zend_Controller_Action {
         $mpdf->WriteHTML($html);
         $mpdf->Output('PDF_Form' . time() . '.pdf', 'D');
         die('End');
+    }
+
+    public function calculatePatientAge($dob) {
+        $dob = date('m/d/Y', strtotime($dob));
+        $tz  = new DateTimeZone('Europe/Brussels');
+        return $age = DateTime::createFromFormat('d/m/Y', $dob, $tz)
+                ->diff(new DateTime('now', $tz))->y;
     }
 
 }
