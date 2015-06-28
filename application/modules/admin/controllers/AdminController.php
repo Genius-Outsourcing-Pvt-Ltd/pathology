a4b3d97802f4bf64b2f9fc915179c9ea2982b965
bilalshakeel <medeveloper2@gmail.com>
2015-06-28 12:32:39 +0500

    results

--- application/modules/admin/controllers/AdminController.php
+++ application/modules/admin/controllers/AdminController.php
@@ -1,21 +1,35 @@
 <?php
-
+use Application_Model_Patient as patient;
 class Admin_AdminController extends Zend_Controller_Action {
 
     public function indexAction() {
+        $model = new Application_Model_Patient();
+        $patients = $model->getAllPatient();
+        $this->view->patients = $patients;
+    }
+    
+    public function vieworderAction() {
+        $id = $this->getRequest()->getParam('id', '');
+        $patient = patient::getPatientById($id);
+//        echo '<pre>'; print_r($patient); die;
+        $this->view->patient = $patient;
+    }
+    
+    public function saveresultAction() {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
         
-    }
-
-    public function manageuserAction() {
-        $model = new Application_Model_User();
-        $select = $model->getallUser();
-        $this->view->data = $this->_helper->Paginator($select);
-    }
-
-    public function changepasswordAction() {
-        $forms = Zend_Registry::get('forms');
-        $form = new Zend_Form($forms->acl->changepassword);
-        $this->view->form = $form;
+        $testId = $this->getRequest()->getParam('testId', '');
+        $result = $this->getRequest()->getParam('result', '');
+        $orderId = $this->getRequest()->getParam('orderId', '');
+        if(!empty($testId) && !empty($result)){
+            $orderTest = new Application_Model_DbTable_OrderTests();
+            $orderTest->update(['results'=>$result], "test_id=$testId AND order_id=$orderId");
+            echo json_encode(['response'=>'success']);
+            die;
+        }
+        echo json_encode(['response'=>'error']);
+        die;
     }
 
 }
---
+++ application/modules/patient/controllers/PatientController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Admin_PatientController extends Zend_Controller_Action {
+
+    public function indexAction() {
+        
+    }
+
+}
